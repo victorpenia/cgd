b39f6702d2cef450ff447132a6313322a5eba359
victorpena <victorpena@192.168.0.102>
2016-10-17 21:51:40 -0400

    first commit

--- app/Http/Controllers/ActivitiesController.php
+++ app/Http/Controllers/ActivitiesController.php
@@ -13,7 +13,7 @@ class ActivitiesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index()//change
     {
     }
     
